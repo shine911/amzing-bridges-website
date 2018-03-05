668dca3a409923a9e6d6f39552ae0cffcf686772
Huỳnh Đình Quí <deathbook1999@gmail.com>
2018-03-06 01:02:08 +0700

    Update infomation
    - Added 3 Bridges
    - Fixed some mistake
    - Fixed Zoom maps

--- components/bridge.js
+++ components/bridge.js
@@ -5,7 +5,7 @@ angular.module('spa-Application').component('bridge', {
   {
   	this.$onInit = function()
   	{
-	  	var mymap = L.map('mapid').setView([this.bridge.x, this.bridge.y], 10);
+	  	var mymap = L.map('mapid').setView([this.bridge.x, this.bridge.y], 15);
 	    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	    maxZoom: 18,
